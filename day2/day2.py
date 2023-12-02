07dedeb1c310ca89b0e6009a6a04abd23cb3bc50
Kristian Numme <k.numme05@gmail.com>
2023-12-02 13:04:47 +0100

    part 1

---
+++ day2/day2.py
@@ -0,0 +1,19 @@
+
+f = open("day2.txt")
+
+list = f.read().splitlines()
+
+gametotal = 0
+
+for line in list:
+    cont = False
+    line = line.split(" ")
+    for i in range(len(line)):
+        if "red" in line[i] and int(line[i-1]) > 12: cont = True
+        if "green" in line[i] and int(line[i-1]) > 13: cont = True
+        if "blue" in line[i] and int(line[i-1]) > 14: cont = True
+
+    if cont: continue
+    gametotal += int(line[1].strip(":"))
+            
+print(gametotal)
