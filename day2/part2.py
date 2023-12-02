ad4858d4ec85b92f7a81a535070ad59a4b8294c0
Kristian Numme <k.numme05@gmail.com>
2023-12-02 13:12:06 +0100

    day 2 finish

---
+++ day2/part2.py
@@ -0,0 +1,20 @@
+
+f = open("day2.txt")
+
+list = f.read().splitlines()
+
+gametotal = 0
+
+for line in list:
+    max_R, max_G, max_B = 0, 0, 0
+    cont = False
+    line = line.split(" ")
+    for i in range(len(line)):
+        if "red" in line[i] and int(line[i-1]) > max_R: max_R = int(line[i-1])
+        if "green" in line[i] and int(line[i-1]) > max_G: max_G = int(line[i-1])
+        if "blue" in line[i] and int(line[i-1]) > max_B: max_B = int(line[i-1])
+
+    power = max_B * max_G * max_R
+    gametotal += power
+            
+print(gametotal)
