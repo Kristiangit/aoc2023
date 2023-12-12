531cf6f7eaad708e8a3222fb761e233f55902fc7
Kristian Numme <95479930+Kristiangit@users.noreply.github.com>
2023-12-12 14:32:42 +0100

    part2 finishi but takes too long

--- day8/2.py
+++ day8/2.py
@@ -6,29 +6,38 @@ list = f.read().splitlines()
 node_dict = {}
 
 total_steps = 0
+currNodes = []
 
 for string in list:
     string = string.split(" ")
     if len(string) < 2:
-        if "L" in string[0]:
-            recipe = string[0]
-            print(recipe, len(recipe))
+        if "L" in string[0]: recipe = string[0]
+
         continue
 
     node_dict[string[0]] = [string[2].strip("(").strip(","), string[-1].strip(")")]
+    if string[0][-1] == "A":
+        currNodes.append(string[0])
     ...
-
-currNode = "AAA"
-while currNode != "ZZZ":
+# print(currNodes)
+all_Z = False
+while not all_Z:
+    z_counter = 0
     now_step = recipe[total_steps % len(recipe)]
     if now_step == "L":
         now_step = 0
     else: now_step = 1
-
-    print(currNode)
-    currNode = node_dict[currNode][now_step]
     total_steps += 1
-    ...
 
-print(currNode)
+    for i in range(len(currNodes)):
+
+        currNodes[i] = node_dict[currNodes[i]][now_step]
+        if (currNodes[i])[-1] == "Z":
+            z_counter += 1
+
+    if z_counter == len(currNodes):
+            all_Z = True
+
+
+
 print(total_steps)
--- test.py
+++ test.py
@@ -12,8 +12,8 @@ for buh in range( buh+5):
 
 # print(80253814 > 66573358)
 print(246640038 < 246587276)
-print(0 % 2)
 
+print(type("j") == str)
 
 # if 343 in range(0,555):
 #     print("yes")
